da0dd505437c757f2ee4a75f550d08beb010c5f5
Teppo Kurki <teppo.kurki@iki.fi>
2015-09-07 21:09:30 +0300

    Start using webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    ui: './lib/ui/main.js',
+    vendor: ['react', 'debug']
+  },
+  output: {
+    path: 'dist',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel'},
+      {test: /\.json$/, loader: 'json'}
+    ]
+  },
+  resolve: {
+    alias: {
+      bacon: "baconjs"
+    }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+  ]
+}
